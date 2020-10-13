6dedfcfc742e3cb66eb0f5c39dbf003b86f9f97a
Darío Lodeiros <dario@aloxa.eu>
2020-10-13 12:02:03 +0200

    [ADD] Demo data

--- pms/__manifest__.py
+++ pms/__manifest__.py
@@ -63,6 +63,10 @@
         "views/product_template_views.xml",
         "views/webclient_templates.xml",
     ],
-    "demo": ["demo/pms_master_data.xml", "demo/pms_reservation.xml"],
+    "demo": [
+    	"demo/pms_master_data.xml",
+    	"demo/pms_reservation.xml",
+    	"demo/pms_folio.xml",
+    	],
     "qweb": ["static/src/xml/pms_base_templates.xml",],
 }
