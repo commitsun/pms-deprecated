b3b49e2fefa93bd0df2387376c66c5dfef802531
padomi <miguel.padin.dominguez@gmail.com>
2020-10-16 20:00:31 +0200

    [TEST] modify class to check when a room is deleted / deactivated / inserted, its type is updated, modifying default_max_avail as long as it is higher than the total number of rooms for that type. Now it considers -1 like a valid value.

--- pms/tests/test_pms_room_type.py
+++ pms/tests/test_pms_room_type.py
@@ -1,17 +1,41 @@
 from odoo.tests import common, tagged
 class TestPmsRoomType(common.TransactionCase):
 
-    def test_update_default_max_avail(self):
+    def test_update_default_max_avail001(self):
 
         #ARRANGE
-        record_room_type = self.browse_ref('pms.pms_room_type_2')
-        rooms = self.env['pms.room'].search([('room_type_id', '=',record_room_type.id)])
-        record_room_type.default_max_avail = record_room_type.total_rooms_count
+        room_type_set = self.env['pms.room.type'].search([('total_rooms_count', '>=', 2)])
+        room_type_record = room_type_set[0]
+
+        rooms_set = self.env['pms.room'].search([
+            ('room_type_id', '=',room_type_record.id),
+            ('active', '=',True)])
 
         #ACT
-        if rooms:
-            rooms[0].active = False
+        rooms_set[0].active = False
 
         #ASSERT
-        self.assertLessEqual(record_room_type.default_max_avail, record_room_type.total_rooms_count,
-        "The default maximum availability is greater than the total room count for a type")
+        self.assertLessEqual(room_type_record.total_rooms_count \
+            if room_type_record.default_max_avail == -1 else \
+                room_type_record.default_max_avail , room_type_record.total_rooms_count,
+        "The default maximum avail. is greater than the total room count for the room type")
+
+    def test_update_default_max_avail002(self):
+
+        #ARRANGE
+        room_type_set = self.env['pms.room.type'].search([('total_rooms_count', '>=', 2)])
+        room_type_record = room_type_set[0]
+
+        self.env['pms.room'].create({
+            'name': 'Test Room 1',
+            'room_type_id': room_type_record.id
+        }).create({
+            'name': 'Test Room 2',
+            'room_type_id': room_type_record.id
+        })
+
+        #ASSERT
+        self.assertLessEqual(room_type_record.total_rooms_count \
+            if room_type_record.default_max_avail == -1 else \
+                room_type_record.default_max_avail , room_type_record.total_rooms_count,
+        "The default maximum avail. is greater than the total room count for the room type")
