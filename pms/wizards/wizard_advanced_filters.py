9c37a44431cf7b07b776377f8a6a05066e056ece
Brais Abeijón <a17braisab@iessanclemente.net>
2020-12-29 18:30:04 +0100

    [REF] pms: change of multiple buttons for one button

--- pms/wizards/wizard_advanced_filters.py
+++ pms/wizards/wizard_advanced_filters.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import _, fields, models
+from odoo.exceptions import UserError
 from odoo.tools.safe_eval import safe_eval
 
 PMS_BUSINESS_MODELS = [
@@ -32,44 +33,24 @@ class AdvancedFiltersWizard(models.TransientModel):
     )
     pms_domain = fields.Char(string="Domain")
 
-    def action_search_reservation(self):
+    def action_filter(self):
         self.ensure_one()
-        domain = safe_eval(self.pms_domain)
-        action = self.env.ref("pms.open_pms_reservation_form_tree_all").read()[0]
-        action["domain"] = domain
-        return action
-
-    def action_search_checkins(self):
-        self.ensure_one()
-        domain = safe_eval(self.pms_domain)
-        action = self.env.ref("pms.action_checkin_partner").read()[0]
-        action["domain"] = domain
-        return action
-
-    def action_search_services(self):
-        self.ensure_one()
-        domain = safe_eval(self.pms_domain)
-        action = self.env.ref("pms.action_service_line").read()[0]
-        action["domain"] = domain
-        return action
-
-    def action_search_folios(self):
-        self.ensure_one()
-        domain = safe_eval(self.pms_domain)
-        action = self.env.ref("pms.open_pms_folio1_form_tree_all").read()[0]
-        action["domain"] = domain
-        return action
-
-    def action_search_invoices(self):
-        self.ensure_one()
-        domain = safe_eval(self.pms_domain)
-        action = self.env.ref("account.action_move_out_invoice_type").read()[0]
-        action["domain"] = domain
-        return action
-
-    def action_search_payments(self):
-        self.ensure_one()
-        domain = safe_eval(self.pms_domain)
-        action = self.env.ref("account.action_account_payments").read()[0]
-        action["domain"] = domain
-        return action
+        if self.pms_model_name == "pms.reservation":
+            action = self.env.ref("pms.open_pms_reservation_form_tree_all").read()[0]
+        if self.pms_model_name == "pms.checkin.partner":
+            action = self.env.ref("pms.action_checkin_partner").read()[0]
+        if self.pms_model_name == "pms.service.line":
+            action = self.env.ref("pms.action_service_line").read()[0]
+        if self.pms_model_name == "pms.folio":
+            action = self.env.ref("pms.open_pms_folio1_form_tree_all").read()[0]
+        if self.pms_model_name == "account.move":
+            action = self.env.ref("account.action_move_out_invoice_type").read()[0]
+        if self.pms_model_name == "account.payment":
+            action = self.env.ref("account.action_account_payments").read()[0]
+        domain = self.pms_domain
+        if domain is not False:
+            domain = safe_eval(self.pms_domain)
+            action["domain"] = domain
+            return action
+        else:
+            raise UserError(_("You must add filters to perform the search"))
