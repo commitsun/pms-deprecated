ff18d00ac9bd8c88173c6bb134141822a126adff
Darío Lodeiros <dario@commitsun.com>
2021-01-24 08:19:24 +0100

    [IMP] pms_l10n_es: Guidelines module name

--- pms-l10n-es/models/pms_property.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class PmsProperty(models.Model):
-    _inherit = "pms.property"
-
-    police_number = fields.Char('Police number', size=10
-                                )
