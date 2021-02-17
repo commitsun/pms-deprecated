4a89913afda6099bd8b04b81f7c4212d807ecce2
Sara Lago <saralago126@gmail.com>
2021-02-17 12:53:24 +0100

    [IMP] Pms: Add property in reservation create in tests and demo data

--- pms/tests/test_pms_folio.py
+++ pms/tests/test_pms_folio.py
@@ -87,6 +87,7 @@ class TestPmsFolio(TestHotel):
 
     def test_commission_and_partner_correct(self):
         # ARRANGE
+        self.create_common_scenario()
         PmsFolio = self.env["pms.folio"]
         PmsReservation = self.env["pms.reservation"]
         PmsPartner = self.env["res.partner"]
@@ -110,6 +111,7 @@ class TestPmsFolio(TestHotel):
                 "checkin": datetime.datetime.now(),
                 "checkout": datetime.datetime.now() + datetime.timedelta(days=3),
                 "agency_id": agency.id,
+                "pms_property_id":self.property.id,
             }
         )
         folio = PmsFolio.create(
