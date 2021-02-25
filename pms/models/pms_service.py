da4611aea8bd3078df270c6f524bdb58d776d2d8
Darío Lodeiros <dario@commitsun.com>
2021-02-25 19:51:55 +0100

    [WIP]pms: pricelist boardservice sql

--- pms/models/pms_service.py
+++ pms/models/pms_service.py
@@ -450,9 +450,7 @@ class PmsService(models.Model):
             return product.price
         final_price, rule_id = origin.pricelist_id.with_context(
             product._context
-        ).get_product_price_rule(
-            product, self.product_qty or 1.0, origin.partner_id
-        )
+        ).get_product_price_rule(product, self.product_qty or 1.0, origin.partner_id)
         base_price, currency_id = self.with_context(
             product._context
         )._get_real_price_currency(
@@ -474,28 +472,39 @@ class PmsService(models.Model):
 
     def _get_real_price_currency(self, product, rule_id, qty, uom, pricelist_id):
         """Retrieve the price before applying the pricelist
-            :param obj product: object of current product record
-            :parem float qty: total quantity of product
-            :param tuple price_and_rule: tuple(price, suitable_rule) coming from pricelist computation
-            :param obj uom: unit of measure of current order line
-            :param integer pricelist_id: pricelist id of sales order"""
-        PricelistItem = self.env['product.pricelist.item']
-        field_name = 'lst_price'
+        :param obj product: object of current product record
+        :parem float qty: total quantity of product
+        :param tuple price_and_rule: tuple(price, suitable_rule) coming from pricelist computation
+        :param obj uom: unit of measure of current order line
+        :param integer pricelist_id: pricelist id of sales order"""
+        PricelistItem = self.env["product.pricelist.item"]
+        field_name = "lst_price"
         currency_id = None
         product_currency = product.currency_id
         if rule_id:
             pricelist_item = PricelistItem.browse(rule_id)
-            if pricelist_item.pricelist_id.discount_policy == 'without_discount':
-                while pricelist_item.base == 'pricelist' and pricelist_item.base_pricelist_id and pricelist_item.base_pricelist_id.discount_policy == 'without_discount':
-                    price, rule_id = pricelist_item.base_pricelist_id.with_context(uom=uom.id).get_product_price_rule(product, qty, self.order_id.partner_id)
+            if pricelist_item.pricelist_id.discount_policy == "without_discount":
+                while (
+                    pricelist_item.base == "pricelist"
+                    and pricelist_item.base_pricelist_id
+                    and pricelist_item.base_pricelist_id.discount_policy
+                    == "without_discount"
+                ):
+                    price, rule_id = pricelist_item.base_pricelist_id.with_context(
+                        uom=uom.id
+                    ).get_product_price_rule(product, qty, self.order_id.partner_id)
                     pricelist_item = PricelistItem.browse(rule_id)
 
-            if pricelist_item.base == 'standard_price':
-                field_name = 'standard_price'
+            if pricelist_item.base == "standard_price":
+                field_name = "standard_price"
                 product_currency = product.cost_currency_id
-            elif pricelist_item.base == 'pricelist' and pricelist_item.base_pricelist_id:
-                field_name = 'price'
-                product = product.with_context(pricelist=pricelist_item.base_pricelist_id.id)
+            elif (
+                pricelist_item.base == "pricelist" and pricelist_item.base_pricelist_id
+            ):
+                field_name = "price"
+                product = product.with_context(
+                    pricelist=pricelist_item.base_pricelist_id.id
+                )
                 product_currency = pricelist_item.base_pricelist_id.currency_id
             currency_id = pricelist_item.pricelist_id.currency_id
 
@@ -506,9 +515,14 @@ class PmsService(models.Model):
             if currency_id.id == product_currency.id:
                 cur_factor = 1.0
             else:
-                cur_factor = currency_id._get_conversion_rate(product_currency, currency_id, self.company_id or self.env.company, self.folio_id.date_order or fields.Date.today())
+                cur_factor = currency_id._get_conversion_rate(
+                    product_currency,
+                    currency_id,
+                    self.company_id or self.env.company,
+                    self.folio_id.date_order or fields.Date.today(),
+                )
 
-        product_uom = self.env.context.get('uom') or product.uom_id.id
+        product_uom = self.env.context.get("uom") or product.uom_id.id
         if uom and uom.id != product_uom:
             # the unit price is in a different uom
             uom_factor = uom._compute_price(1.0, product.uom_id)
@@ -553,7 +567,7 @@ class PmsService(models.Model):
                 product_context["date_overnight"] = date
             if reservation and self.is_board_service:
                 product_context["board_service"] = reservation.board_service_room_id.id
-            product = self.product_id.with_context(product_context)            
+            product = self.product_id.with_context(product_context)
             return self.env["account.tax"]._fix_tax_included_price_company(
                 self._get_display_price(product),
                 product.taxes_id,
